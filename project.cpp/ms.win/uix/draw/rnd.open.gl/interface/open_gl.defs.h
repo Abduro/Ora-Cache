ebeb9729d004c7c0694a3941089d751e55c1b74c
Abduro <ebontrop@gmail.com>
2025-08-09 01:25:30 +0700

    OpenGL draw renderer lib project stub is created;

---
+++ project.cpp/ms.win/uix/draw/rnd.open.gl/interface/open_gl.defs.h
@@ -0,0 +1,16 @@
+#ifndef _OPEN_GL_DEFS_H_INCLUDED
+#define _OPEN_GL_DEFS_H_INCLUDED
+/*
+	Created by Tech_dog (ebontrop@gmail.com) on 08-Aug-2025 at 23:56:27.465, (UTC+4) Batumi, Friday;
+	This is Ebo Pack OpenGL based draw renderer generic definitions' declaration file;
+*/
+#include <atlbase.h>
+#include <gl/gl.h>
+
+#pragma comment(lib, "opengl32.lib")
+
+namespace ex_ui { namespace draw { namespace open_gl {
+
+}}}
+
+#endif/*_OPEN_GL_DEFS_H_INCLUDED*/
---
+++ project.cpp/ms.win/uix/draw/rnd.open.gl/src/open_gl.defs.cpp
@@ -0,0 +1,9 @@
+/*
+	Created by Tech_dog (ebontrop@gmail.com) on 08-Aug-2025 at 23:58:27.843, (UTC+4) Batumi, Friday;
+	This is Ebo Pack OpenGL based draw renderer generic definitions' implementation file;
+*/
+#include "open_gl.defs.h"
+
+using namespace ex_ui::draw::open_gl;
+
+namespace ex_ui { namespace draw { namespace open_gl { namespace _impl { void __warning_lnk_4221(void) {}}}}}
