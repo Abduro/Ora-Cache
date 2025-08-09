9e887682b4a4ef4160b265134bf8d15eebc0d41f
Abduro <ebontrop@gmail.com>
2025-08-09 13:45:06 +0700

    Graphics trackball user control project is created;

---
+++ project.cpp/ms.win.ctl/sfx.g.track/interface/sfx.trkball.ctrl.h
@@ -0,0 +1,10 @@
+#ifndef _SFX_TRACK_CTRL_H_INCLUDED
+#define _SFX_TRACK_CTRL_H_INCLUDED
+/*
+	Created by Tech_dog (ebontrop@gmail.com) on 09-Aug-2025 at 04:24:01.580, UTC+4, Batumi, Saturday;
+	This is Ebo Pack trackball user control interface declaration file;
+*/
+
+namespace ex_ui { namespace controls { namespace sfx { namespace track {
+}}}}
+#endif/*_SFX_TRACK_CTRL_H_INCLUDED*/
---
+++ project.cpp/ms.win.ctl/sfx.g.track/src/sfx.trkball.ctrl.cpp
@@ -0,0 +1,7 @@
+/*
+	Created by Tech_dog (ebontrop@gmail.com) on 09-Aug-2025 at 06:18:03.111, UTC+4, Batumi, Saturday;
+	This is Ebo Pack trackball user control interface implementation file;
+*/
+#include "sfx.trkball.ctrl.h"
+
+using namespace ex_ui::controls::sfx::track;
