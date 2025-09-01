7df0a16ff9d43cc787ea30522ec513cfe78765ea
Abduro <ebontrop@gmail.com>
2025-09-01 18:41:46 +0700

    OpenGL tutorials solution: creating draw context in tutorial project (stage #1);
    (1) Created the device context  (aka fake) for OpenGL drawing renderer;

--- project.cpp.tutors/open_gl/open_gl.shared/interface/gl_context.h
+++ project.cpp.tutors/open_gl/open_gl.shared/interface/gl_context.h
@@ -24,26 +24,63 @@ namespace context {
 	private:
 		 CBase& operator = (const CBase&) = delete; CBase& operator = (CBase&&) = delete;
 	protected:
+		 mutable // for updating the error state in 'const' methods, properties;
 		 CError_ex m_error;
 	};
+
+	class CTarget : public CBase {
+	public:
+		 CTarget (void);  CTarget (const HWND); CTarget (const CTarget&) = delete; CTarget (CTarget&&) = delete;
+		~CTarget (void);
+
+		const HDC Get (void) const; // gets target window device context handle;
+		err_code  Set (const HWND); // sets target window device context handle;
+
+		bool Is_valid (void) const; // validates device context handle;
+		err_code  Free(void) ;      // releases the device context and sets error to '__e_not_inited', otherwise the error state of the failure;
+
+		CTarget& operator <<(const HWND);  // invokes this::Set(...); if the device context is already obtained, it will be released first;
+
+	protected:
+		HDC  m_dc_src ; // this is the source device context from which the device renderer is created; it is auto-released on destroying this class instance;
+		HWND m_target ; // the target window handle, it owns the source device context; this window handle is saved for releasing the HDC;
+	};
+
 	// https://learn.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-wglcreatecontext ;
 	// excerpt: ...creates a new OpenGL rendering context, which is suitable for drawing on the device referenced by hdc... ;
 	class CDevice : public CBase {
 	public:
-		 CDevice (void);
+		 CDevice (void); CDevice (const HWND);
 		~CDevice (void);
 
 		err_code Create (const HWND); // creates the rendering context that is compatible with input window device context;
 		err_code Destroy(void);
 
+		bool   Is_valid (void) const; // returns 'true' in case if device renderer context is not not nullptr; no target object check;
+		HGLRC  Renderer (void) const;
+
+		const
+		CTarget& Target (void) const;
+		CTarget& Target (void) ;       // releasing device context directly in target object will lead to errors on operations with the renderer;
+
+		CDevice& operator <<(const HWND); // invokes this::Create(...); if the renderer context is already created, it will be destroyed first;
+
 	private:
-		HDC    m_dc_src ; // this is the source device context from which the device renderer is created;
+		CTarget m_target; // this is the object containing the target window handle and its device context one;
 		HGLRC  m_drw_ctx; // rendering context that is compatible with regular GDI;
-		HWND   m_target ; // the target window handle, it owns the source device context;
+	};
+}
+
+	class CContext : public context::CBase { typedef context::CBase TBase;
+	public:
+		 CContext (void) ;  CContext (const CContext&) = delete; CContext (CContext&&) = delete;
+		~CContext (void) ;
+
+	private:
+		 CContext& operator = (const CContext&) = delete; CContext& operator = (CContext&&) = delete;
+	
 	};
 
 }}}
-
-}
 
 #endif/*_GL_CONTEXT_H_INCLUDED*/
--- project.cpp.tutors/open_gl/open_gl.shared/src/gl_context.cpp
+++ project.cpp.tutors/open_gl/open_gl.shared/src/gl_context.cpp
@@ -22,15 +22,51 @@ TErr_ex& context::CBase::Error (void) const { return this->m_error; }
 
 /////////////////////////////////////////////////////////////////////////////
 
-context::CDevice:: CDevice (void) : CBase(), m_drw_ctx(0), m_dc_src(0), m_target(0) { CBase::m_error <<__CLASS__; }
-context::CDevice::~CDevice (void) {}
+context::CTarget:: CTarget (void) : CBase(), m_dc_src(0), m_target(0) { CBase::m_error <<__CLASS__; }
+context::CTarget:: CTarget (const HWND _h_wnd) : CTarget() { *this << _h_wnd; }
+context::CTarget::~CTarget (void) {
+	this->Free ();
+}
 
-err_code context::CDevice::Create (const HWND _h_wnd) {
+err_code  context::CTarget::Free(void) {
+	// https://stackoverflow.com/questions/3945721/win32-why-does-releasedc-say-my-dc-is-not-found ; good answer is >>
+	// https://stackoverflow.com/a/3948202/4325555 ; the excerpt : ...Lots of OpenGl samples do rather (to my mind) weired things with device contexts... ;
+	CBase::m_error << __METHOD__ << __s_ok;
+
+	if (this->Is_valid() == false) {
+		this->m_dc_src = nullptr; // for cases when handle of the device context has invalid state, i.e. is closed outside of this class instance;
+		return CBase::Error();    // returns possibly the error state that is set by this::Is_valid();
+	}
+
+	if (0 == ::ReleaseDC(this->m_target, this->m_dc_src)) {
+		CBase::m_error << (err_code) TErrCodes::eExecute::eState = _T("Releasing source device context failed");
+		__trace_err_3(_T("%s"), (_pc_sz) CBase::Error().Print(TError::e_req));
+	}
+
+	return CBase::Error();
+}
+
+bool context::CTarget::Is_valid (void) const {
+
+	if (nullptr == this->m_dc_src)
+		return false != (CBase::m_error << __e_not_inited).Is(); // the error onject set to error state (i.e. error == true);
+
+	// https://learn.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-getobjecttype ;
+	const uint32_t u_dc_type = ::GetObjectType(this->m_dc_src);
+
+	if (OBJ_DC != u_dc_type && OBJ_MEMDC != u_dc_type) // not sure that the source device context may be an in-memory one, but anyway it is okay;
+		return false != (CBase::m_error << __e_inv_arg = _T("Source device context is not valid")).Is();
+
+	return false == CBase::Error().Is(); // returns 'true' due to the error state is not set to err_code, i.e. everything is '__s_ok';
+}
+
+const HDC context::CTarget::Get (void) const { return this->m_dc_src; }
+err_code  context::CTarget::Set (const HWND _h_wnd) {
 	_h_wnd;
-	CBase::m_error <<__METHOD__<<__s_ok;
+	CBase::m_error << __METHOD__ << __s_ok;
 
-	if (this->m_drw_ctx) {
-		CBase::m_error << (err_code) TErrCodes::eObject::eInited = _T("Draw renderer exists");
+	if (this->Is_valid()) {
+		CBase::m_error << (err_code) TErrCodes::eObject::eInited = _T("Source device context is already set");
 		__trace_err_3(_T("%s"), (_pc_sz) CBase::Error().Print(TError::e_req)); return CBase::Error();
 	}
 
@@ -38,10 +74,44 @@ err_code context::CDevice::Create (const HWND _h_wnd) {
 		CBase::m_error << __e_hwnd = _T("Target window handle is invalid");
 		__trace_err_3(_T("%s"), (_pc_sz) CBase::Error().Print(TError::e_req)); return CBase::Error();
 	}
+
 	this->m_target = _h_wnd;
 	this->m_dc_src = ::GetDC(_h_wnd);
 	__empty_ln();
 	__trace_info_3(_T("context device: handle=%s\n"), (_pc_sz)__address_of(this->m_dc_src));
+
+	return CBase::Error();
+}
+
+CTarget&  context::CTarget::operator <<(const HWND _h_wnd) {
+	_h_wnd;
+	if (this->Is_valid())
+		this->Free();
+	if (this->Error()) // releasing the source device context handle is failed;
+		return *this;
+	this->Set(_h_wnd); return *this; // for getting the result of retreiving the device context the error state should be checked;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+context::CDevice:: CDevice (void) : CBase() { CBase::m_error <<__CLASS__; }
+
+context::CDevice:: CDevice (const HWND _h_wnd) : CDevice() { *this << _h_wnd; }
+
+context::CDevice::~CDevice (void) { this->Destroy(); }
+
+err_code context::CDevice::Create (const HWND _h_wnd) {
+	_h_wnd;
+	CBase::m_error <<__METHOD__<<__s_ok;
+
+	if (this->Is_valid()) {
+		CBase::m_error << (err_code) TErrCodes::eObject::eInited = _T("Draw renderer exists");
+		__trace_err_3(_T("%s"), (_pc_sz) CBase::Error().Print(TError::e_req)); return CBase::Error();
+	}
+
+	if (__failed(this->Target().Set(_h_wnd))) {
+		return CBase::m_error = this->Target().Error();
+	}
 
 	PIXELFORMATDESCRIPTOR px_fmt_desc = {0}; // https://learn.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-pixelformatdescriptor ;
 	// https://www.khronos.org/opengl/wiki/Creating_an_OpenGL_Context_(WGL) << there is the example of how to do that;
@@ -54,26 +124,26 @@ err_code context::CDevice::Create (const HWND _h_wnd) {
 	px_fmt_desc.cDepthBits = 24; // the number of bits for the depthbuffer;
 
 	// https://learn.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-choosepixelformat ;
-	int32_t n_px_format = ::ChoosePixelFormat(this->m_dc_src, &px_fmt_desc);
+	int32_t n_px_format = ::ChoosePixelFormat(this->Target().Get(), &px_fmt_desc);
 	if (0== n_px_format) {
 		__trace_err_3(_T("%s"), (_pc_sz) (CBase::m_error()(CError::e_cmds::e_get_last)).Print(TError::e_req));
 		return CBase::Error();
 	}
 
 	// https://learn.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-setpixelformat ;
-	if (false == !!::SetPixelFormat(this->m_dc_src, n_px_format, &px_fmt_desc)) {
+	if (false == !!::SetPixelFormat(this->Target().Get(), n_px_format, &px_fmt_desc)) {
 		__trace_err_3(_T("%s"), (_pc_sz) (CBase::m_error()(CError::e_cmds::e_get_last)).Print(TError::e_req));
 		return CBase::Error();
 	}
 
 	// https://learn.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-wglcreatecontext ;
-	this->m_drw_ctx = ::wglCreateContext(this->m_dc_src);
+	this->m_drw_ctx = ::wglCreateContext(this->Target().Get());
 	if ( 0 == this->m_drw_ctx) {
 		__trace_err_3(_T("%s"), (_pc_sz) (CBase::m_error()(CError::e_cmds::e_get_last)).Print(TError::e_req));
 		return CBase::Error();
 	}
 
-	if (0 == ::wglMakeCurrent(this->m_dc_src, this->m_drw_ctx)) { // it is required, otherwise nothing will work;
+	if (0 == ::wglMakeCurrent(this->Target().Get(), this->m_drw_ctx)) { // it is required, otherwise nothing will work;
 		__trace_err_3(_T("%s"), (_pc_sz) (CBase::m_error()(CError::e_cmds::e_get_last)).Print(TError::e_req));
 	}
 	return CBase::Error();
@@ -85,7 +155,7 @@ err_code context::CDevice::Destroy(void) {
 	// (1) destroys the renderer handle first;
 	if (nullptr != this->m_drw_ctx) {
 		// https://learn.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-wglmakecurrent ;
-		if (0 == ::wglMakeCurrent(m_dc_src, 0)) {
+		if (0 == ::wglMakeCurrent(this->Target().Get(), 0)) {
 			__trace_err_3(_T("%s"), (_pc_sz) (CBase::m_error()(CError::e_cmds::e_get_last)).Print(TError::e_req));
 		}
 		// https://learn.microsoft.com/en-us/windows/win32/api/wingdi/nf-wingdi-wgldeletecontext ;
@@ -96,13 +166,35 @@ err_code context::CDevice::Destroy(void) {
 			this->m_drw_ctx = nullptr;
 	}
 	// (2) destroys device context handle; https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-releasedc ;
-	if (nullptr != this->m_dc_src) {
-		if (0 == ::ReleaseDC(this->m_target, this->m_dc_src)) {
-			CBase::m_error << (err_code) TErrCodes::eExecute::eState = _T("Releasing source device context failed");
-			__trace_err_3(_T("%s"), (_pc_sz) CBase::Error().Print(TError::e_req));
-		}
-			
-		this->m_dc_src = nullptr; // sets nullptr regardless the error of releasing device context;
-	}
+	if (this->Target().Is_valid())
+		if (__failed(this->Target().Free()))
+			CBase::m_error = this->Target().Error();
+
 	return CBase::Error();
 }
+
+bool   context::CDevice::Is_valid (void) const {
+	return !!this->m_drw_ctx;
+}
+
+HGLRC  context::CDevice::Renderer (void) const {
+	return this->m_drw_ctx;
+}
+
+const
+context::CTarget& context::CDevice::Target (void) const { return this->m_target; }
+context::CTarget& context::CDevice::Target (void)       { return this->m_target; }
+
+CDevice& context::CDevice::operator <<(const HWND _h_wnd) {
+	_h_wnd;
+	if (this->Is_valid())
+		this->Destroy();
+	if (this->Error()) // the destruction is failed;
+		return *this;
+	this->Create(_h_wnd); return *this; // for getting the result of creation the error state should be checked;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+CContext:: CContext (void) { TBase::m_error >>__CLASS__<<__METHOD__<< __e_not_inited; }
+CContext::~CContext (void) {}
