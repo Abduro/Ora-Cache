e04f6a1d21efc37bc2b072bea795276bb4e31862
Abduro <ebontrop@gmail.com>
2026-08-18 08:22:07 +0700

    Stage #203: creating WPF test cases for vertual camera;
    (1) The project is still being under implementation;
    (2) The project icon is created, draft version, but usable;

---
+++ project.net.tutors/wpf.virt.cam.app/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Virtual Camera Test App")]
+[assembly: AssemblyDescription("This is a project for testing virtual camera.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Abu Ebo")]
+[assembly: AssemblyProduct(".Net Runtime Front End")]
+[assembly: AssemblyCopyright("Tech_dog © 2019-2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("00000C13-EEEE-BBBB-6667-0000001D0007")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
