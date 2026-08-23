6bf2d6e7bae8bcf00ed8582f76d59299761b8150
Abduro <ebontrop@gmail.com>
2026-08-23 20:38:14 +0700

    Stage #207: creating vertual camera test projects (C++);
    (1) Test project for virtual camera ray is added;

---
+++ project.cpp.tutors/virt.cam.tests/test.0.common/src/resources/com.test.res.h
@@ -0,0 +1,13 @@
+/*
+	Created by Tech_dog (ebontrop@gmail.com) on 23-Aug-2026 at 15:33:00.075, UTC+4, Batumi, Sunday;
+	This is virtual camera tests' common resource declaration file.
+*/
+
+#pragma region __frame
+
+#define IDR_APP_ICO 1001
+#define IDS_APP_CAP 1003
+
+#pragma endregion
+
+
