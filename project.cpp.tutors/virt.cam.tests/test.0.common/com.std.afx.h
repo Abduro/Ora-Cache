3295b48bfc0a1e87050100926bf1a231c0ea85f2
Abduro <ebontrop@gmail.com>
2026-08-24 13:17:02 +0700

    Stage #208: creating vertual camera test projects (C++);
    (1) All test cases' project stubs are created;
    (2) Desktop GUI test app project is created too;

--- project.cpp.tutors/virt.cam.tests/test.0.common/com.std.afx.h
+++ project.cpp.tutors/virt.cam.tests/test.0.common/com.std.afx.h
@@ -27,6 +27,8 @@
 #pragma warning(disable: 4481)  // nonstandard extension used: override specifier 'override';
 #pragma warning(disable: 4996)  // security warning: function or variable may be unsafe;
 
+#include <atlbase.h>
+
 #ifdef _DEBUG
 	#define _CRTDBG_MAP_ALLOC
 	#include <stdlib.h>
